2c67a08775f66ca9003f0eb24c05e54e93601a52
Pascal1010 <plluberes@student.bridgew.edu>
2022-10-03 19:09:26 -0400

    making a pumpkin

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import arcade
+pumpkin_window = arcade.open_window(1000, 900, "Jack-o-lantern")
+arcade.set_background_color(arcade.color.PURPLE)
+arcade.start_render()
+arcade.draw_circle_filled(500, 450, 300, arcade.color.PUMPKIN)
+arcade.draw_triangle_filled(425, 375, 500, 475, 575, 375, arcade.color.BLACK)
+arcade.draw_arc_outline(500, 374, 400, 100, arcade.color.BLACK, -180, 0, 7)
+arcade.draw_xywh_rectangle_filled(220, 740, 560, 20, arcade.color.BLACK)
+arcade.draw_xywh_rectangle_filled(330, 760, 330, 100, arcade.color.BLACK)
+arcade.draw_arc_filled(330, 495, 100, 200, arcade.color.BLACK, 0, 180)
+arcade.draw_arc_filled(660, 495, 100, 200, arcade.color.BLACK, 0, 180)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+arcade.finish_render()
+arcade.run()
